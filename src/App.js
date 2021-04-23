51ccb5af3fcb009a5fb779b3f65c5f9481dd8714
Hdonge <harshal.donge@gmail.com>
2021-04-23 15:47:08 +0530

    Added info text to dashboard

--- src/App.js
+++ src/App.js
@@ -6,6 +6,8 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+        <p>This is React Js App</p>
+        <p>We created this app to test CI/CD Pipelines using Netlify and Budddy.works</p>
       </header>
     </div>
   );
