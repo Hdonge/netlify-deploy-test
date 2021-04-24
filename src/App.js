6e92a15374325d0b8927c78f64b7045507fc7ccb
Hdonge <harshal.donge@gmail.com>
2021-04-24 17:34:39 +0530

    Added a commit

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>This is ReactJs App</p>
-        <p>We created this app to test CI/CD Pipelines using Netlify and Budddy.works</p>
+        <p>We created this app to test CI/CD Pipelines using Netlify and Budddy Works</p>
       </header>
     </div>
   );
